0644ded3e8fc504dc2c6cd602f6420d780bcd54b
Jack Jiang <jackjyq@outlook.com>
2018-05-22 00:33:52 +1000

    submission 5:
    # svm:
    linear, C=1.0
    # feature
    - bernoulli model
    - dynamic making 20 changes (depend on the impact on weight)
    # other
    the feature is implemented by CountVectorizer

--- Jack/submission_back.py
+++ Jack/submission_back.py
@@ -2,7 +2,6 @@ import helper
 from collections import defaultdict
 import numpy as np
 from sklearn.model_selection import GridSearchCV
-from sklearn.feature_extraction.text import CountVectorizer
 
 ################################ debug functions ###############################
 def debug(*argv):
@@ -25,25 +24,16 @@ def debug_matrix(title, matrix):
 
 
 ############################## train data functions ############################
-# def get_vocabulary(strategy_instance):
-#     # generate a sorted list contains all words in all samples
-#     word_set = set()
-#     for sample in strategy_instance.class0:
-#         for word in sample:
-#             word_set.add(word)
-#     for sample in strategy_instance.class1:
-#         for word in sample:
-#             word_set.add(word)
-#     vocabulary = sorted(list(word_set))
-#     return vocabulary
-
-
-def get_vocabulary(vocabulary_):
-    vocabulary = [None for _ in range(len(vocabulary_))]
-    for token in vocabulary_:
-        index = vocabulary_[token]
-        vocabulary[index] =  token
-    # print(vocabulary)
+def get_vocabulary(strategy_instance):
+    # generate a sorted list contains all words in all samples
+    word_set = set()
+    for sample in strategy_instance.class0:
+        for word in sample:
+            word_set.add(word)
+    for sample in strategy_instance.class1:
+        for word in sample:
+            word_set.add(word)
+    vocabulary = sorted(list(word_set))
     return vocabulary
 
 
@@ -73,16 +63,11 @@ def get_feature_matrix(input_matrix, vocabulary):
     return output_matrix
 
 
-def get_x_train(strategy_instance):
-    corpus = []
-    for para in strategy_instance.class0 + strategy_instance.class1:
-        corpus.append(' '.join(para))
-    # print(corpus)
-    vectorizer = CountVectorizer(binary=True)
-    x_train = vectorizer.fit_transform(corpus).toarray()
-    # print(x_train[0])
-    return x_train, vectorizer
-
+def get_x_train(strategy_instance, vocabulary):
+    word_matrix = strategy_instance.class0 + strategy_instance.class1
+    x_matrix = get_feature_matrix(word_matrix, vocabulary)
+    x_train = np.array(x_matrix)
+    return x_train
 
 
 def get_y_train(strategy_instance):
@@ -93,17 +78,16 @@ def get_y_train(strategy_instance):
     return y_train
 
 
-def get_prediction(clf, file_path, vectorizer):
+def get_prediction(clf, file_path, vocabulary):
     # print out the index of wrong answer
     # generate feature
-    corpus = []
     with open(file_path,'r') as fh:
-        for para in fh:
-            corpus.append(para)
-    x_test = vectorizer.transform(corpus).toarray()
-    y_test = clf.predict(x_test)
-    y_df = clf.decision_function(x_test)
-    return y_test, y_df
+        word_matrix=[line.strip().split(' ') for line in fh]
+    x_matrix = get_feature_matrix(word_matrix, vocabulary)
+    x_train = np.array(x_matrix)
+    prediction = clf.predict(x_train)
+    decision_function = clf.decision_function(x_train)
+    return prediction, decision_function
 
 
 ############################# modify file functions ############################
@@ -175,7 +159,7 @@ def get_modified_vector(input_vector, weight_dict, vocabulary, n):
         #
         weight_sum = weight_dict[class1_vector[i_1]]\
                      + weight_dict[class0_vocabulary[i_0]]
-        if weight_sum > 0:
+        if weight_sum < 0:
             # rm  class1_vector[i_1]
             i_1 += 1
         else:
@@ -206,17 +190,18 @@ def write_to_file(input_matrix ,file_path):
 
 
 ################################ debug functions ###############################
-def show_test_result(clf, vectorizer):
+def show_test_result(clf, vocabulary):
+    dim = len(vocabulary)
     # get prediction
-    y0, df0 = get_prediction(clf, './class-0.txt', vectorizer)
-    y1, df1 = get_prediction(clf, './class-1.txt', vectorizer)
-    y_test,df_test = get_prediction(clf, './test_data.txt', vectorizer)
-    y_mod, df_mod = get_prediction(clf, './modified_data.txt', vectorizer)
+    prediction0, decision_function0 = get_prediction(clf, './class-0.txt', vocabulary)
+    prediction1, decision_function1 = get_prediction(clf, './class-1.txt', vocabulary)
+    prediction_test,decision_function_test = get_prediction(clf, './test_data.txt', vocabulary)
+    prediction_mod, decision_function_mod = get_prediction(clf, './modified_data.txt', vocabulary)
     # calculate success rate
-    rate0 = y0.tolist().count(0) / y0.shape[0] * 100
-    rate1 = y1.tolist().count(1) / y1.shape[0] * 100
-    rate_test = y_test.tolist().count(1)/y_test.shape[0] * 100
-    rate_mod = y_mod.tolist().count(0)/y_mod.shape[0] * 100
+    rate0 = prediction0.tolist().count(0) / prediction0.shape[0] * 100
+    rate1 = prediction1.tolist().count(1) / prediction1.shape[0] * 100
+    rate_test = prediction_test.tolist().count(1)/prediction_test.shape[0] * 100
+    rate_mod = prediction_mod.tolist().count(0)/prediction_mod.shape[0] * 100
     # print test result
     print('########################## Test Result ############################')
     print(str(clf))
@@ -228,14 +213,14 @@ def show_test_result(clf, vectorizer):
     print('############################# Detail ##############################')
     # print('class-0 prediction =\n' + str(prediction0))
     # print('class-1 prediction =\n' + str(prediction1))
-    # print('test_data prediction =\n' + str(prediction_test))
+    print('test_data prediction =\n' + str(prediction_test))
     # print('modiefied_data prediction =\n' + str(prediction_mod))
     # print('class-0 decision_function =\n' + str(decision_function0))
     # print('class-1 decision_function =\n' + str(decision_function1))
-    print('test_data decision_function =\n' + str(df_test))
-    # print('test_data sum = ', sum(decision_function_test))
-    print('modiefied_data decision_function =\n' + str(df_mod))
-    # print('modiefied_data sum = ', sum(decision_function_mod))
+    print('test_data decision_function =\n' + str(decision_function_test))
+    print('test_data sum = ', sum(decision_function_test))
+    print('modiefied_data decision_function =\n' + str(decision_function_mod))
+    print('modiefied_data sum = ', sum(decision_function_mod))
 
 ################################ fool_classifier ###############################
 def fool_classifier(test_data): ## Please do not change the function defination...
@@ -247,7 +232,7 @@ def fool_classifier(test_data): ## Please do not change the function defination.
     ########################### define parameter ###########################
     n = 20     # the number of distinct words that can be modified
     parameters={'gamma': 'auto',
-                'C': 1.0,
+                'C': 0.1,
                 'kernel': 'linear',
                 'degree': 3,
                 'coef0': 0.0
@@ -271,38 +256,33 @@ def fool_classifier(test_data): ## Please do not change the function defination.
     #    Independent term in kernel function.
     #    It is only significant in 'poly' and 'sigmoid'.
 
-
-
     ############################# train data ###############################
     # debug_matrix('class0', strategy_instance.class0)
     # debug_matrix('class1', strategy_instance.class1)
     # get vocabulary
-    # vocabulary = get_vocabulary(strategy_instance)
-    # # debug('vocabulary =\n', vocabulary)
-    # # get y_train
+    vocabulary = get_vocabulary(strategy_instance)
+    # debug('vocabulary =\n', vocabulary)
+    # get y_train
     y_train = get_y_train(strategy_instance)
-    # debug('y_train =\n', y_train)
-    # # get x_train
-    x_train, vectorizer = get_x_train(strategy_instance)
-    # # debug('x_train =\n', x_train)
-    # # training
-    clf = strategy_instance.train_svm(parameters, x_train, y_train)
-    # debug(clf)
-    # param_range = np.arange(0.001,1,0.01)
-    # param_grid = [{'C': param_range, 'kernel': ['linear']}]
-    # grid = GridSearchCV(clf_start, param_grid)
-    # grid.fit(x_train,y_train)
-    # clf = grid.best_estimator_
-    # ############################# modify file ##############################
-    # # read test_data.txt
+    debug('y_train =\n', y_train)
+    # get x_train
+    x_train = get_x_train(strategy_instance, vocabulary)
+    # debug('x_train =\n', x_train)
+    # training
+    clf_start = strategy_instance.train_svm(parameters, x_train, y_train)
+    param_range = np.arange(0.001,1,0.01)
+    param_grid = [{'C': param_range, 'kernel': ['linear']}]
+    grid = GridSearchCV(clf_start, param_grid)
+    grid.fit(x_train,y_train)
+    clf = grid.best_estimator_
+    ############################# modify file ##############################
+    # read test_data.txt
     test_data_matrix = read_to_matrix(test_data)
     # debug_matrix('test_data_matrix', test_data_matrix)
-    vocabulary = get_vocabulary(vectorizer.vocabulary_)
-    # debug('vocabulary =\n', vocabulary)
-    # # get weight_list
+    # get weight_list
     weight_list = clf.coef_.tolist()[0]
     # debug('weight_list =\n', weight_list)
-    # # get weight_dict
+    # get weight_dict
     weight_dict = get_weight_dict(weight_list, vocabulary)
     # debug_dict('weight_dict', weight_dict)
     # get modified matrix
@@ -317,7 +297,7 @@ def fool_classifier(test_data): ## Please do not change the function defination.
     # Check that the modified text is within the modification limits.
     assert strategy_instance.check_data(test_data, modified_data)
     # Show test result
-    # show_test_result(clf, vectorizer)
+    show_test_result(clf, vocabulary)
     return strategy_instance ## NOTE: You are required to return the instance of this class.
 
 
