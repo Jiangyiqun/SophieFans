3c4de8fbefb1c1642b09a3c2603fd9d1011aa297
Jack Jiang <jackjyq@outlook.com>
2018-05-22 15:39:58 +1000

    submit 7:
    rewrite the code, fix some  bugs

--- Jack/submission.py
+++ Jack/submission.py
@@ -6,7 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 ################################ debug functions ###############################
 def debug(*argv):
-    if True:
+    if False:
         print(*argv)
 
 
@@ -25,54 +25,6 @@ def debug_matrix(title, matrix):
 
 
 ############################## train data functions ############################
-# def get_vocabulary(strategy_instance):
-#     # generate a sorted list contains all words in all samples
-#     word_set = set()
-#     for sample in strategy_instance.class0:
-#         for word in sample:
-#             word_set.add(word)
-#     for sample in strategy_instance.class1:
-#         for word in sample:
-#             word_set.add(word)
-#     vocabulary = sorted(list(word_set))
-#     return vocabulary
-
-
-def get_vocabulary(vocabulary_):
-    vocabulary = [None for _ in range(len(vocabulary_))]
-    for token in vocabulary_:
-        index = vocabulary_[token]
-        vocabulary[index] =  token
-    # print(vocabulary)
-    return vocabulary
-
-
-def get_feature_vector(input_vector, vocabulary):
-    dim = len(vocabulary)
-    output_vector = [0 for i in range(dim)]
-    for word in input_vector:
-        try:
-            i = vocabulary.index(word)
-            output_vector[i] = 1
-        except ValueError:
-            pass    # remains 0 when the word is not in word_table
-    return output_vector
-
-
-def get_feature_matrix(input_matrix, vocabulary):
-    # arguments: 
-    #   input_matrix = [ input_list = [word]]
-    # return:
-    #   output_matrix = [ output_list = [existence of word]]
-    #   based on vocabulary
-    #
-    output_matrix = list()
-    for input_vector in input_matrix:
-        output_vector = get_feature_vector(input_vector, vocabulary)
-        output_matrix.append(output_vector)
-    return output_matrix
-
-
 def get_x_train(strategy_instance):
     corpus = []
     for para in strategy_instance.class0 + strategy_instance.class1:
@@ -84,7 +36,6 @@ def get_x_train(strategy_instance):
     return x_train, vectorizer
 
 
-
 def get_y_train(strategy_instance):
     len0 = len(strategy_instance.class0)
     len1 = len(strategy_instance.class1)
@@ -93,20 +44,16 @@ def get_y_train(strategy_instance):
     return y_train
 
 
-def get_prediction(clf, file_path, vectorizer):
-    # print out the index of wrong answer
-    # generate feature
-    corpus = []
-    with open(file_path,'r') as fh:
-        for para in fh:
-            corpus.append(para)
-    x_test = vectorizer.transform(corpus).toarray()
-    y_test = clf.predict(x_test)
-    y_df = clf.decision_function(x_test)
-    return y_test, y_df
-
-
 ############################# modify file functions ############################
+def get_vocabulary(vocabulary_):
+    vocabulary = [None for _ in range(len(vocabulary_))]
+    for token in vocabulary_:
+        index = vocabulary_[token]
+        vocabulary[index] =  token
+    # print(vocabulary)
+    return vocabulary
+
+
 def get_weight_dict(weight_list, vocabulary):
     weight_dict = defaultdict(float)
     for i in range(len(vocabulary)):
@@ -121,71 +68,84 @@ def read_to_matrix(file_path):
     return data_matrix
 
 
-def get_class1_vector(input_vector, weight_dict):
-    # remove duplicated words
-    cleaned_vector = list(set(input_vector))
-    # sort based on weight (from class 1 to class 0)
-    combined_vector = []
-    for word in cleaned_vector:
-        combined_vector.append((word, weight_dict[word]))
-    sorted_vector = sorted(combined_vector,key=lambda l:l[1], reverse=True)
-    # unzip and generate output_vector
-    output_vector = []
-    for pair in sorted_vector:
-        output_vector.append(pair[0])
-    return output_vector
+def get_feature_matrix(data_matrix, vectorizer):
+    corpus = []
+    for para in data_matrix:
+        corpus.append(' '.join(para))
+    # print(corpus)
+    feature_matrix = vectorizer.transform(corpus).toarray().tolist()
+    # print(x_train[0])
+    return feature_matrix
 
 
-def get_class0_vocabulary(input_vector, weight_dict, vocabulary):
-    # remove words in vocabulary which are in input_vector as well
-    cleaned_vector = list(set(vocabulary) - set(input_vector))
-    # sort based on weight (from class 0 to class 1)
-    combined_vector = []
-    for word in cleaned_vector:
-        combined_vector.append((word, weight_dict[word]))
-    sorted_vector = sorted(combined_vector,key=lambda l:l[1])
-    # unzip and generate output_vector
-    output_vector = []
-    for pair in sorted_vector:
-        output_vector.append(pair[0])
-    return output_vector
-
-
-def get_modified_vector(input_vector, weight_dict, vocabulary, n):
-    # change class1_vector base on class0_vocabulary
-    class1_vector = get_class1_vector(input_vector, weight_dict)
-    class0_vocabulary =\
-            get_class0_vocabulary(input_vector, weight_dict, vocabulary)
-    i_1 = 0     # counter of class1_vector
-    i_0 = 0     # counter of class0_vocabulary
-    output_vector = []
+def get_to_modified_words(feature_vector, weight_list, vocabulary, n):
+    to_rm_candidates = []   # all index of words in feature_vector
+    to_rm_weight = []
+    to_add_candidates = []  # all index of words not in feature_vector
+    to_add_weight = []
+    to_rm_index = []        # the index need to be removed
+    to_add_index = []       # the index need to be removed
+    to_rm_words = []        # the word need to be removed
+    to_add_words = []       # the word need to be removed
+    # split data_feature into two lists
+    for i in range(len(feature_vector)):
+        if feature_vector[i] == 1:
+            to_rm_candidates.append(i)
+            to_rm_weight.append(weight_list[i])
+        else:   # feature_vector[i] == 0
+            to_add_candidates.append(i)
+            to_add_weight.append(weight_list[i])
+    # sort by weight
+    to_rm_weight, to_rm_candidates =\
+            zip(*sorted(zip(to_rm_weight, to_rm_candidates), reverse=True))
+    to_add_weight, to_add_candidates =\
+            zip(*sorted(zip(to_add_weight, to_add_candidates)))
+    # debug('to_rm_candidates =\n', to_rm_candidates)
+    # debug('to_rm_weight =\n', to_rm_weight)
+    # debug('to_add_candidates =\n', to_add_candidates)
+    # debug('to_add_weight =\n', to_add_weight)
+    # dynamically utilise to_rm_candidates and to_add_candidates
+    i_rm = 0      # pointer in to_rm_weight & to_rm_candidates
+    i_add = 0     # pointer in to_add_weight & to_add_candidates
     # perform n time distinct change
-    for i in range(n):
-        # eg.   how can we get from class1 to class0
-        # class1_vector[i_1]               weight_sum
-        #           class0_vocabulary[i_0]              choice
-        #      1           -2                 <0    add class0_vocabulary[i_0]
-        #      1           -0.5               >0    rm class1_vector[i_1]
-        #      1           0.5                >0    rm  class1_vector[i_1]
-        #      1           2                  >0    class1_vector[i_1]
-        #      -1          -2                 <0    add class0_vocabulary[i_0]
-        #      -1          -0.5               <0    add class0_vocabulary[i_0]
-        #      -1          0.5                <0    add class0_vocabulary[i_0]
-        #      -1          2                  >0    rm  class1_vector[i_1]
+    for _ in range(n):
+        #          how can we get from class1 to class0
+        # to_rm_weight[i_rm]                weight_sum
+        #           to_add_weight[i_add]              best choice
+        #      1           -0.5               >0            rm
+        #      1           0.5                >0            rm
+        #      1           2                  >0            rm
+        #      -1          2                  >0            rm 
+        #      -1          -2                 <0            add 
+        #      -1          -0.5               <0            add
+        #      -1          0.5                <0            add
+        #      1           -2                 <0            add
         #
-        weight_sum = weight_dict[class1_vector[i_1]]\
-                     + weight_dict[class0_vocabulary[i_0]]
+        weight_sum = to_rm_weight[i_rm] + to_add_weight[i_add]
         if weight_sum > 0:
-            # rm  class1_vector[i_1]
-            i_1 += 1
+            # rm
+            to_rm_index.append(to_rm_candidates[i_rm])
+            i_rm += 1
         else:
-            # add class0_vocabulary[i_0]
-            output_vector.append(class0_vocabulary[i_0])
-            i_0 += 1
-    # add remained items to output_vector
-    for i in range(i_1, len(class1_vector)):
-        output_vector.append(class1_vector[i])
-    return output_vector
+            # add
+            to_add_index.append(to_add_candidates[i_add])
+            i_add += 1
+    # change index to word
+    for i in to_rm_index:
+        to_rm_words.append(vocabulary[i])
+    for i in to_add_index:
+        to_add_words.append(vocabulary[i])
+    return to_rm_words, to_add_words
+
+
+def get_modified_vector(data_vector, to_rm_words, to_add_words):
+    modified_vector = []
+    for word in data_vector:
+        if word not in to_rm_words:
+            modified_vector.append(word)
+    for word in to_add_words:
+        modified_vector.append(word)
+    return modified_vector
 
 
 def get_modified_matrix(input_matrix, weight_dict, vocabulary, n):
@@ -199,13 +159,27 @@ def get_modified_matrix(input_matrix, weight_dict, vocabulary, n):
 
 
 def write_to_file(input_matrix ,file_path):
-    with open(file_path, 'w') as fh:
+    with open(file_path, 'w+') as fh:
         for input_vector in input_matrix:
             line = ' '.join(input_vector) + '\n'
             fh.write(line)
 
 
 ################################ debug functions ###############################
+def get_prediction(clf, file_path, vectorizer):
+    # print out the index of wrong answer
+    # generate feature
+    corpus = []
+    with open(file_path,'r') as fh:
+        for para in fh:
+            corpus.append(para)
+    x_test = vectorizer.transform(corpus).toarray()
+    y_test = clf.predict(x_test)
+    y_df = clf.decision_function(x_test)
+    return y_test, y_df
+
+
+
 def show_test_result(clf, vectorizer):
     # get prediction
     y0, df0 = get_prediction(clf, './class-0.txt', vectorizer)
@@ -236,6 +210,12 @@ def show_test_result(clf, vectorizer):
     # print('test_data sum = ', sum(decision_function_test))
     print('modiefied_data decision_function =\n' + str(df_mod))
     # print('modiefied_data sum = ', sum(decision_function_mod))
+
+def log_change(to_rm_words, to_add_words):
+    with open('./log_change.txt', 'a+') as fh:
+        fh.write('Removed: ' + str(to_rm_words)\
+                + "   Added: " + str(to_add_words) + '\n')
+
 
 ################################ fool_classifier ###############################
 def fool_classifier(test_data): ## Please do not change the function defination...
@@ -271,50 +251,56 @@ def fool_classifier(test_data): ## Please do not change the function defination.
     #    Independent term in kernel function.
     #    It is only significant in 'poly' and 'sigmoid'.
 
-
-
     ############################# train dkata ###############################
     # debug_matrix('class0', strategy_instance.class0)
     # debug_matrix('class1', strategy_instance.class1)
-    # get vocabulary
-    # vocabulary = get_vocabulary(strategy_instance)
-    # # debug('vocabulary =\n', vocabulary)
-    # # get y_train
     y_train = get_y_train(strategy_instance)
     # debug('y_train =\n', y_train)
-    # # get x_train
+
     x_train, vectorizer = get_x_train(strategy_instance)
     # debug('x_train =\n', x_train)
-    # # training
+
+    # training
     clf = strategy_instance.train_svm(parameters, x_train, y_train)
     # debug(clf)
+
+    # grid search
     # param_range = [2**i for i in range(-100, 100)]
     # param_grid = [{'C': param_range, 'kernel': ['linear']}]
     # grid = GridSearchCV(clf_start, param_grid)
     # grid.fit(x_train,y_train)
     # clf = grid.best_estimator_
-    # ############################# modify file ##############################
-    # # read test_data.txt
-    test_data_matrix = read_to_matrix(test_data)
-    # debug_matrix('test_data_matrix', test_data_matrix)
-    vocabulary = get_vocabulary(vectorizer.vocabulary_)
-    # debug('vocabulary =\n', vocabulary)
 
-    # # get weight_list
+    # ############################# modify file ##############################
+    vocabulary = get_vocabulary(vectorizer.vocabulary_)
+    debug('vocabulary =\n', vocabulary)
+
     weight_list = clf.coef_.tolist()[0]
-    # debug('weight_list =\n', weight_list)
-    # for i in range(len(vocabulary)):
-    #     print(vocabulary[i], weight_list[i])
-    # # get weight_dict
-    weight_dict = get_weight_dict(weight_list, vocabulary)
-    # debug_dict('weight_dict', weight_dict)
+    debug('weight_list =\n', weight_list)
+
+    data_matrix = read_to_matrix(test_data)
+    # debug_matrix('data_matrix =\n', data_matrix)
+
+    feature_matrix = get_feature_matrix(data_matrix, vectorizer)
+    # debug_matrix('feature_matrix =\n', feature_matrix)
+
     # get modified matrix
-    modified_data_matrix =\
-            get_modified_matrix(test_data_matrix, weight_dict, vocabulary, n)
-    # debug_matrix('modified_data_matrix', modified_data_matrix)
+    modified_matrix = []
+    for i in range(len(data_matrix)):
+        data_vector = data_matrix[i]
+        feature_vector = feature_matrix[i]
+        to_rm_words, to_add_words =\
+                get_to_modified_words(feature_vector, weight_list, vocabulary, n)
+        # log_change(to_rm_words, to_add_words)
+        modified_vector = get_modified_vector(data_vector,\
+                to_rm_words, to_add_words)
+        # debug('modified_vector =\n', modified_vector)
+        modified_matrix.append(modified_vector.copy())
+    # debug_matrix('modified_matrix', modified_matrix)
+
     # write to modified_data
     modified_data='./modified_data.txt'
-    write_to_file(modified_data_matrix ,modified_data)
+    write_to_file(modified_matrix ,modified_data)
 
     ################################## test  ###################################
     # Check that the modified text is within the modification limits.
